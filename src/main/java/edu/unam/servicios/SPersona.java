ea7a5b18810537a7b635ec3406f74750585f2444
Bruno2204 <brunoegama@gmail.com>
2023-07-13 01:19:48 -0300

    Changes: PersonaABM

--- src/main/java/edu/unam/servicios/SPersona.java
+++ src/main/java/edu/unam/servicios/SPersona.java
@@ -6,6 +6,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -24,12 +25,16 @@ public class SPersona implements Initializable {
     @FXML
     TableView<Persona> tableView;
     @FXML
+    Button btRemove;
+    @FXML
+    Button btModify;
+    @FXML
     private TableColumn colNombre;
     @FXML
     private TableColumn colApellido;
     @FXML
     private TableColumn colCUIL;
-    public ObservableList<Persona> personas = FXCollections.observableArrayList();
+    public ObservableList<Persona> personas;
 
     public void agregar(ActionEvent e){
         String nombre = nameField.getText();
@@ -39,15 +44,47 @@ public class SPersona implements Initializable {
         personas.add(p);
         this.tableView.setItems(personas);
     }
-    public void eliminar(){
-        System.out.println(personas.get(0).nombre);
-    }
 
+
+    public void eliminar() {
+
+        // Obtengo la persona seleccionada
+        Persona p = tableView.getSelectionModel().getSelectedItem();
+
+        // Si la persona es nula, lanzo error
+        if (p != null) {
+            personas.remove(p);
+            tableView.refresh();
+        }
+
+    }
+    public void botonChanges(){
+        Persona p = tableView.getSelectionModel().getSelectedItem();
+        if (p != null){
+            btModify.setDisable(false);
+            btRemove.setDisable(false);
+            nameField.setText(p.nombre);
+            surnameField.setText(p.apellido);
+            cuilField.setText(p.cuil);
+        }
+    }
+    public void modificar(){
+        Persona p = tableView.getSelectionModel().getSelectedItem();
+        String nombre = nameField.getText();
+        String apellido = surnameField.getText();
+        String cuil = cuilField.getText();
+
+        p.nombre = nombre;
+        p.apellido = apellido;
+        p.cuil = cuil;
+
+        tableView.refresh();
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
-        this.colNombre.setCellValueFactory(new PropertyValueFactory("Nombre"));
-        this.colApellido.setCellValueFactory(new PropertyValueFactory("Apellido"));
-        this.colCUIL.setCellValueFactory(new PropertyValueFactory("cuil"));
+        personas = FXCollections.observableArrayList();
+        colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+        colApellido.setCellValueFactory(new PropertyValueFactory("apellido"));
+        colCUIL.setCellValueFactory(new PropertyValueFactory("cuil"));
     }
 }
